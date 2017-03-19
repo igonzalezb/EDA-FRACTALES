d0a4e11a427efccf93aaa5816df5db49c5398879
Matias <mpierdominici@itba.edu.ar>
2017-03-19 11:49:44 -0300

    header de triangulo .c terminado

--- TP2- FRACTALES/TP2- FRACTALES/triangulo.h
+++ TP2- FRACTALES/TP2- FRACTALES/triangulo.h
@@ -1,12 +1,12 @@
 
 #ifndef TRIANGULO_H
 #define TRIANGULO_H
+
 typedef struct {
-	float x;
-	float y;
+	float x;//coordenada del eje x
+	float y;//coordenada del eje y
 }point_t;
 
 void TriangleRecursion(point_t StartPosition, float lStart, float lEnd, float RightAngle, float LeftAngle);
-
 
 #endif // _TRIANGULO_H
