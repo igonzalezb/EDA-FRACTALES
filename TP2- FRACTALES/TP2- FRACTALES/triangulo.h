7c244bb9f407e200d71ea0525f52feca2f1319e7
Matias <mpierdominici@itba.edu.ar>
2017-03-19 22:50:50 -0300

    terminado

--- TP2- FRACTALES/TP2- FRACTALES/triangulo.h
+++ TP2- FRACTALES/TP2- FRACTALES/triangulo.h
@@ -7,6 +7,7 @@ typedef struct {
 	float y;//coordenada del eje y
 }point_t;
 
-void TriangleRecursion(point_t StartPosition, float lStart, float lEnd, float RightAngle, float LeftAngle);
+void TriangleRecursion(point_t StartPosition, float lStart, float lEnd, float RightAngle, float LeftAngle, int16_t ScreenWidth, int16_t ScreenHigh);
+
 
 #endif // _TRIANGULO_H
